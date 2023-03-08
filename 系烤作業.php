d0e2ed34e5769e885c491cbb90447ab7e3770568
XinYing <125635433+A1103357@users.noreply.github.com>
2023-03-08 13:32:07 +0800

    0223HW

---
+++ 系烤作業.php
@@ -0,0 +1,47 @@
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>該夯肉了吧</title>
+</head>
+
+<body background="https://img.lovepik.com/58pic/32/62/32/97Q58PICx8qs0e36gI89f_PIC2018.png">
+<A NAME="top"></A>
+<font size="5" color="#2c332b">根據英國研究統計顯示，每年固定有烤肉的人，幸福感和</font><br/>
+<font size="5" color="#2c332b">增胖率通常是別人的兩倍</font><br/>
+<font size="5" color="#2c332b">為了全資管的幸福，</font>
+<font size="5" color="red" >來烤肉吧！</font><br/>
+<p>
+<font size="5" color="#2c332b">關於烤肉有哪些食材...</font><br/>
+<table border="3">
+<tr><th>順序</th><th>食材</th></tr>
+<tr><td>1</td><td>梅花豬</td></tr>
+<tr><td>2</td><td>牛五花</td></tr>
+<tr><td>3</td><td>新鮮時蔬</td></tr>
+<tr><td>4</td><td>嫩雞柳</td></tr>
+<tr><th>\</th><th>飲料</th></tr>
+<tr><td>1</td><td>百四可樂</td></tr>
+<tr><td>2</td><td>雪畢</td></tr>
+<tr><td>3</td><td>有糖綠茶不加糖</td></tr>
+<tr><td>4</td><td>沒有柳橙的柳橙汁</td></tr>
+</table>
+<font size="5" color="#2c332b">餓了吧？快報名！</font><br/>
+<P>
+<br/>
+<font size="5" color="#2c332b">-------以下為報名表單-------</font><br/>
+
+<form method="post" action="meatresult.php">
+請輸入學號：<input type="text" name="id" required><br/>  
+請輸入姓名：<input type="text" name="name" required><br/>
+是否願意參加系烤：想參加<input type="radio" name="yes" required>不想也得想<input type="radio" name="yes"><br/>
+請勾選您欲參加的日期：<br/>
+02/30<input type="radio" name="day" value="0230" required>
+03/14<input type="radio" name="day" value="0314">
+03/16<input type="radio" name="day" value="0316"><br/>
+<input type="submit">
+<input type="reset">
+</form>
+<font size="5" color="#2c332b">-------報名表單結束-------</font><br/>
+<a HREF=#top>回去看烤肉食材<br/></a>
+</body>
+</html>
